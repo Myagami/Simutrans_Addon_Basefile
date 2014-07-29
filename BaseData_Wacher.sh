837bd3f1c9a9cb8fda2cf30befd7c7ba74b2edf7
M.Yagami <hayate@Reinforce-Eins.(none)>
2014-07-29 14:16:50 +0900

    Update for 14/07/29 14:16:50

---
+++ BaseData_Wacher.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+while inotifywait -e modify -e create -e delete -r ./ ; do
+    #追加ファイルを確認
+    git add * ;
+    #今日の日付
+    Today='';
+    Today=`date +"%y/%m/%d %T"`;
+    #commit
+    git commit -m "Update for $Today" ;
+    #to github
+    git push github master ;
+done
